45e3a976e9fcd6877fc4e2c7f8e6fd20653c3d32
chuck <chuck.schrumpf@daily-harvest.com>
2020-04-10 12:50:18 -0400

    hotfix: bug in letter score calculations

--- app/webflow_utils.py
+++ app/webflow_utils.py
@@ -13,23 +13,23 @@ logger = logging.getLogger(__name__)
 def get_letter_score(value):
     if value >= 94:
         return "A"
-    elif 93 >= value >= 90:
+    elif 94 > value >= 90:
         return "A-"
-    elif 89 >= value >= 87:
+    elif 90 > value >= 87:
         return "B+"
-    elif 86 >= value >= 83:
+    elif 87 > value >= 83:
         return "B"
-    elif 82 >= value >= 80:
+    elif 83 > value >= 80:
         return "B-"
-    elif 79 >= value >= 77:
+    elif 80 > value >= 77:
         return "C+"
-    elif 76 >= value >= 73:
+    elif 77 > value >= 73:
         return "C"
-    elif 72 >= value >= 70:
+    elif 73 > value >= 70:
         return "C-"
-    elif 69 >= value >= 67:
+    elif 70 > value >= 67:
         return "D+"
-    elif 66 >= value >= 60:
+    elif 67 > value >= 60:
         return "D"
     else:
         return "F"
